39c24d0f8f7cae3ced5f635322f7355134746477
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-07-31 18:31:11 -0400

    fixed deployment bug. was missing repository name for base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/UTMPC-E-Waste-Management/",
   plugins: [react()],
 })
